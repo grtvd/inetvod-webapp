ff4791db23dd65d56830b589e0fce4268adfe375
grtvd <grtvd@users.noreply.github.com>
2008-07-03 14:12:18 -0400

    webapp: Updated component name for DownloadServiceMgr. Moved location of loading Player.SerialNo from DownloadServiceMgr.

--- scripts/data/Session.js
+++ scripts/data/Session.js
@@ -77,6 +77,10 @@ function Session()
 	this.fPlayer.ModelNo = "webapp";
 	this.fPlayer.SerialNo = "1";
 	this.fPlayer.Version = "1.0.0000";
+
+	this.checkInstall();
+	if (this.fDownloadServiceMgr && this.fDownloadServiceMgr.getPlayerSerialNo())
+		this.fPlayer.SerialNo = this.fDownloadServiceMgr.getPlayerSerialNo();
 }
 
 /******************************************************************************/
@@ -257,9 +261,7 @@ function Session()
 	{
 		try
 		{
-			this.fDownloadServiceMgr = new ActiveXObject("iNetVOD.MCE.Gateway.DownloadServiceMgr");
-
-			this.fPlayer.SerialNo = this.fDownloadServiceMgr.getPlayerSerialNo();
+			this.fDownloadServiceMgr = new ActiveXObject("iNetVOD.DLS.Gateway.DownloadServiceMgr");
 		}
 		catch(ignore) {}
 	}
